276316dbd35732657dba6bd720715bd6b8b2b967
Nathan Thorpe <nathan@techstormpc.com>
2023-10-13 17:45:42 -0700

    added api client and templates

---
+++ src/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BillingApi';
+export * from './DatasetsApi';
+export * from './ExecutionApi';
+export * from './MetricsApi';
+export * from './NotebooksApi';
+export * from './ProcessesApi';
+export * from './ProjectsApi';
+export * from './SystemApi';
+export * from './UsersApi';
