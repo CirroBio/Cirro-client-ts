31bd2f7471f885f75ab86eac500996b2819f6dc4
Nathan Thorpe <nathan@techstormpc.com>
2025-06-30 19:10:29 -0700

    Expand test case for manifest parser

--- packages/sdk/src/file/__test__/manifest-parser.spec.ts
+++ packages/sdk/src/file/__test__/manifest-parser.spec.ts
@@ -1,5 +1,6 @@
 import { DatasetAssetsManifest } from "@cirrobio/api-client";
 import { ManifestParser } from "../manifest-parser";
+import { FileSystemObjectType } from "../models/file-object.model";
 
 
 describe("manifestParser", () => {
@@ -17,36 +18,43 @@ describe("manifestParser", () => {
         path: 'folder1/folder2/file2.txt',
         size: 1234,
         metadata: {
-          description: 'Test file 1',
         }
-      }
+      },
+      {
+        path: 'folder1/folder2/file3.txt',
+        size: 1234,
+        metadata: {
+        }
+      },
     ],
   }
 
   it("should generate assets", () => {
     const parser = new ManifestParser(testManifest);
     const assets = parser.generateAssets();
-    expect(assets.length).toBe(2);
+    expect(assets.length).toBe(3);
     expect(assets[0].url).toBe('s3://example-bucket/datasets/file1.txt');
     expect(assets[0].size).toBe(testManifest.files[0].size);
     expect(assets[0].name).toBe('file1.txt');
     expect(assets[0].id).toBeTruthy();
-    expect(assets.length).toBe(2);
     expect(assets[1].url).toBe('s3://example-bucket/datasets/folder1/folder2/file2.txt');
     expect(assets[1].size).toBe(testManifest.files[0].size);
     expect(assets[1].name).toBe('file2.txt');
     expect(assets[1].path).toBe('folder1/folder2');
     // calculate check
-    expect(assets.totalSizeBytes).toBe(2468);
-    expect(assets.totalSize).toBe("2.4 KiB");
+    expect(assets.totalSizeBytes).toBe(3702);
+    expect(assets.totalSize).toBe("3.6 KiB");
 
   });
 
   it("should generate assets with folders", () => {
     const parser = new ManifestParser(testManifest);
     const assets = parser.generateAssets(true);
-    expect(assets.length).toBe(4);
-    expect(assets[2].name).toBe('folder1');
-    expect(assets[3].name).toBe('folder2');
+    const folders = assets.filter(a => a.type === FileSystemObjectType.FOLDER);
+    expect(folders.length).toBe(2);
+    expect(folders[0].name).toBe('folder1');
+    expect(folders[1].name).toBe('folder2');
+    const files = assets.filter(a => a.type === FileSystemObjectType.FILE);
+    expect(files.length).toBe(3);
   });
 })
