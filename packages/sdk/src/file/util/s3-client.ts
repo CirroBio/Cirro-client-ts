4d83874055eab3bd52ca3805c4db0af5c34f57e7
Nathan Thorpe <nathan@techstormpc.com>
2025-11-12 14:43:30 -0800

    bypass cache for s3 client overrides

--- packages/sdk/src/file/util/s3-client.ts
+++ packages/sdk/src/file/util/s3-client.ts
@@ -11,16 +11,8 @@ const clientDefaults: S3ClientConfigType = {
   followRegionRedirects: true,
 }
 
-/**
- * Creates an S3 client using the provided credentials.
- */
-export function createS3Client(credentials: AWSCredentials, clientOverrides: S3ClientConfigType = {}): S3Client {
-  const cacheKey = `${credentials.accessKeyId}-${credentials.region}`;
-  const cachedClient = clientCache.get(cacheKey);
-  if (cachedClient) {
-    return cachedClient;
-  }
-  const newClient = new S3Client({
+function createS3ClientInternal(credentials: AWSCredentials, clientOverrides: S3ClientConfigType): S3Client {
+  return new S3Client({
     credentials: {
       accessKeyId: credentials.accessKeyId,
       secretAccessKey: credentials.secretAccessKey,
@@ -30,6 +22,22 @@ export function createS3Client(credentials: AWSCredentials, clientOverrides: S3C
     ...clientDefaults,
     ...clientOverrides
   });
+}
+
+/**
+ * Creates an S3 client using the provided credentials.
+ */
+export function createS3Client(credentials: AWSCredentials, clientOverrides: S3ClientConfigType = {}): S3Client {
+  // Bypass cache when creating s3 client with overrides, since these values may be changed.
+  if (Object.keys(clientOverrides).length > 0) {
+    return createS3ClientInternal(credentials, clientOverrides);
+  }
+  const cacheKey = `${credentials.accessKeyId}-${credentials.region}`;
+  const cachedClient = clientCache.get(cacheKey);
+  if (cachedClient) {
+    return cachedClient;
+  }
+  const newClient = createS3ClientInternal(credentials, clientOverrides)
   clientCache.set(cacheKey, newClient);
   return newClient;
 }
